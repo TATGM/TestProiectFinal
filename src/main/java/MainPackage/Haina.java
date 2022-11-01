6819f33cde82343bf2daff1515bfa83b510624a0
FazakasAlex <fazakasalex121@yahoo.com>
2022-11-01 16:47:11 +0200

    Classes updated

---
+++ src/main/java/MainPackage/Haina.java
@@ -0,0 +1,65 @@
+package MainPackage;
+
+public class Haina
+{
+    public static int NR_OF_CLOTHING_ITEMS;
+    private String nume;
+    private String culoare;
+    private int pret;
+
+    public Haina(String nume)
+    {
+        this.nume = nume;
+        NR_OF_CLOTHING_ITEMS++;
+    }
+
+    public Haina(String nume, String culoare, int pret)
+    {
+        this.nume = nume;
+        this.culoare = culoare;
+        this.pret = pret;
+        NR_OF_CLOTHING_ITEMS++;
+    }
+
+    public String getNume()
+    {
+        return nume;
+    }
+
+    public void setNume(String nume)
+    {
+        this.nume = nume;
+    }
+
+    public String getCuloare()
+    {
+        return culoare;
+    }
+
+    public void setCuloare(String culoare)
+    {
+        this.culoare = culoare;
+    }
+
+    public int getPret()
+    {
+        return pret;
+    }
+
+    public void setPret(int pret)
+    {
+        this.pret = pret;
+    }
+
+    public static int getTotalNumber()
+    {
+        return NR_OF_CLOTHING_ITEMS;
+    }
+
+    public String toString()
+    {
+        String s = "Nume: " + nume + "|  Culoare: " + culoare + "|  Pret: " + pret + "|  Numar total de haine = " +NR_OF_CLOTHING_ITEMS;
+        return s;
+    }
+
+}
---
+++ src/main/java/MainPackage/Project.java
@@ -0,0 +1,203 @@
+package MainPackage;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.table.*;
+import java.util.ArrayList;
+
+public class Project implements ActionListener
+{
+    static JTextField text, text2;
+    static JLabel output;
+    static JTable table;
+    public static ArrayList<Haina> haine = new ArrayList<Haina>();
+    //static Haina[] haine = new Haina[20];
+    static DefaultTableModel model;
+
+    public static void main(String[] args) {
+
+        StringBuffer sb=new StringBuffer("Meniu "); 
+        sb.append("Principal"); 
+        JFrame frame = new JFrame(sb.toString()); 
+
+        Haina h1 = new Haina("Tricou", "Rosu", 20);
+        Haina h2 = new Haina("Tricou", "Negru", 25);
+        Haina h3 = new Haina("Bluza", "Albastru", 55);
+        Haina h4 = new Haina("Pantaloni", "Galben", 35);
+        haine.add(h1);
+        haine.add(h2);
+        haine.add(h3);
+        haine.add(h4);
+
+        String[][] data = new String[20][3]; 
+
+        for(int i = 0; i < Haina.NR_OF_CLOTHING_ITEMS ;i++)
+        {
+            data[i][0] = haine.get(i).getNume();
+            data[i][1] = haine.get(i).getCuloare();
+            data[i][2] = Integer.toString(haine.get(i).getPret());
+
+        }
+        
+        String[] columnNames = { "Articol", "Culoare", "Pret" };
+ 
+
+        model = new DefaultTableModel(columnNames, 0);
+
+        table = new JTable(model);
+
+        frame.add(new JScrollPane(table));
+
+        for(int i = 0; i < Haina.NR_OF_CLOTHING_ITEMS ;i++)
+        {
+        model.addRow(
+                   new Object[]{
+                         haine.get(i).getNume(), 
+                         haine.get(i).getCuloare(),
+                         haine.get(i).getPret()
+                   });
+        }
+
+        JButton addButton = new JButton("Add Article");
+        JButton searchButton = new JButton("Cautare");
+
+        frame.add(addButton);frame.add(searchButton);
+        
+        searchButton.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                new Search(haine);
+            }
+        });
+
+        Project instance = new Project();
+
+        addButton.addActionListener(instance);
+
+        frame.setLayout(new FlowLayout());
+        frame.setSize(480, 750);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public void actionPerformed(ActionEvent e){
+        new CreateFrame(); 
+    }
+
+
+class CreateFrame extends JFrame
+{
+    public CreateFrame()
+    {
+
+        final JTextField text1 = new JTextField(10);
+        final JTextField text2 = new JTextField(10);
+        final JTextField text3 = new JTextField(10);
+        final JLabel label1  = new JLabel("Articol: ");
+        final JLabel label2  = new JLabel("Culoare: ");
+        final JLabel label3  = new JLabel("Pret   : ");
+
+        JButton button = new JButton("Done");
+
+        add(label1); add(text1); add(label2); add(text2); add(label3); add(text3);
+
+        add(button);
+        button.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                if(Utility.isNumeric(text1.getText()) || (Utility.isNumeric(text2.getText())) || !(Utility.isNumeric(text3.getText())))
+                {
+                    new ShowError(); 
+                    dispose();
+                }
+                else
+                {
+                Haina h = new Haina(text1.getText(), text2.getText(), Integer.parseInt(text3.getText()));
+                //haine[Haina.NR_OF_CLOTHING_ITEMS] = h;
+                haine.add(h);
+
+                model.addRow(
+                   new Object[]{
+                        text1.getText(), 
+                        text2.getText(),
+                        text3.getText()
+                   });
+                dispose();
+                }
+            }
+        });
+
+        setLayout(new FlowLayout());
+        setSize(500, 100);
+        setVisible(true);
+    }
+}
+}
+
+class ShowError extends JFrame
+{
+    public ShowError()
+    {
+    JLabel erLabel  = new JLabel("EROARE: datele de intrare nu sunt corecte");
+    JButton errorButton = new JButton("Confirm");
+    errorButton.addActionListener(new ActionListener()
+    {
+        public void actionPerformed(ActionEvent e)
+        {
+            dispose();
+        }
+    });
+
+    add(erLabel); add(errorButton);
+    setLayout(new FlowLayout());
+    setSize(400, 100);
+    setVisible(true);
+    }
+}
+
+class Search extends JFrame
+{
+    public Search(final ArrayList<Haina> haine)
+    {
+    final JLabel searchLabel  = new JLabel("Introdu Articolul Cautat");
+    final JLabel articolLabel  = new JLabel("Aici vor aparea informatiile despre articol");
+    final JTextField searchText = new JTextField(10);
+    final JButton searchButton = new JButton("Confirm");
+    searchButton.addActionListener(new ActionListener()
+    {
+        public void actionPerformed(ActionEvent e)
+        {
+            int n = 0;
+            int m = 0;
+            for(int i = 0; i < Haina.NR_OF_CLOTHING_ITEMS ;i++)
+            {
+                if(searchText.getText().equals(haine.get(i).getNume()))
+                {
+                m = i;
+                n++;
+                }
+            }
+            if(n == 0)
+            searchLabel.setText("Nu sa gasit");
+            else if (n == 1)
+            {
+            searchLabel.setText("Gasit!");
+            articolLabel.setText(haine.get(m).toString());
+            }
+            else 
+            {
+            searchLabel.setText("Gasit " + n + " articole numite: ");
+            articolLabel.setText(haine.get(m).toString());
+            }
+        }
+    });
+
+    add(searchLabel); add(searchText); add(searchButton); add(articolLabel);
+    setLayout(new FlowLayout());
+    setSize(500, 200);
+    setVisible(true);
+    }
+}
